8bc3f5ed4125ffd3c414aa6badddd0d459a535e7
Adrien Hannon <adrien_hannon@ens.univ-artois.fr>
2022-05-03 11:38:28 +0200

    Add getData and getModel function

---
+++ experiments/generator.py
@@ -0,0 +1,45 @@
+from revert.transforms import shift_all
+from revert.models import ConvNet, Module, Pipe, View
+
+from torch.utils.data import TensorDataset, DataLoader
+
+import torch 
+
+# ============== Data =============================
+
+def getData(stdev) :
+    data = torch.load("../scripts-pcmri/pcmri_tensor.pt")
+    flows = data["flows"]
+
+    shifted, y = shift_all(stdev)(flows)
+
+    data_dataset = TensorDataset(shifted, y)
+    data_loader = DataLoader(data_dataset, shuffle=True, batch_size=1)
+    
+    return data_loader
+
+
+#==================================================
+
+#--- Models ---
+
+def getModel(Npts = 32) :
+    layers = [[Npts, 6,   8],
+              [16,  6*12,  8],
+              [8,   6*24,  8],
+              [1,   6*12,  1]]
+
+    base = ConvNet(layers, pool='max')
+
+    dim_out = 6*12
+    dim_task = 6
+    head = ConvNet([[1, dim_out, 1], [1, dim_task, 1]]) 
+    
+    #--- loss function ---
+    def mse_loss(y, y_tgt):
+        return ((y - y_tgt)**2).sum()
+    
+    head.loss = lambda y, y_tgt : mse_loss(y, y_tgt)
+    view = View([1,dim_out,1])
+    
+    return Pipe(base, view ,head)
